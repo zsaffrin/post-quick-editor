290de2b0f1c9f79d8065901725eda570e0f1a4c1
Zach Saffrin <zach@zachsaffrin.com>
2017-08-07 23:05:46 -0400

    Reorganize structure, add Delete Post functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,75 @@
 import React, { Component } from 'react';
 
-import {
-	fetchGetJson,
-	updatePost,
-} from './_staticFunctions';
+import { fetchGetJson, updatePost, deletePost } from './_staticFunctions';
 import Post from './Post';
 
 require('font-awesome/css/font-awesome.min.css');
 require('../styles/App.scss');
 
-const defaultState = {
-	appTitle: 'Post Quick-Editor',
-	posts: [],
-};
-
 const WP_URL = 'http://localhost:8888/wp-json/wp/v2';
-const WP_POSTS_API_URL = `${WP_URL}/posts`;
-const WP_MEDIA_API_URL = `${WP_URL}/media`;
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = defaultState;
+		this.state = {
+			appTitle: 'Post Quick-Editor',
+			posts: [],
+			shouldPostsUpdate: true,
+		};
+
+		this.removePost = this.removePost.bind(this);
 	}
 
 	componentDidMount() {
 		this.refreshPosts();
 	}
 
+	componentDidUpdate() {
+		if (this.state.shouldPostsUpdate) {
+			this.refreshPosts();
+		}
+	}
+
 	refreshPosts() {
-		fetchGetJson(WP_POSTS_API_URL)
-			.then(posts => this.setState({ posts }));
+		fetchGetJson(`${WP_URL}/posts`)
+			.then(posts => this.setState({
+				posts,
+				shouldPostsUpdate: false,
+			}));
 	}
 
 	savePost(id, data) {
-		const url = `${WP_POSTS_API_URL}/${id}`;
+		const url = `${WP_URL}/posts/${id}`;
 		updatePost(url, data)
 			.then(() => this.refreshPosts);
+	}
+
+	removePost(id) {
+		const url = `${WP_URL}/posts/${id}`;
+		deletePost(url)
+			.then(() => this.setState({ shouldPostsUpdate: true }));
 	}
 
 	render() {
 		const { appTitle, posts } = this.state;
 
-		let postNodes;
-		if (posts.length > 0) {
-			postNodes = posts.map((post) => {
+		const postNodes = posts ?
+			posts.map((post) => {
 				const { id, title, featured_media } = post;
-
-				const featuredMedia = fetchGetJson(
-					`${WP_MEDIA_API_URL}/${featured_media}`,
-				)
-					.then(media => media.source_url);
 
 				return (
 					<Post
 						id={id}
 						title={title.rendered}
-						featuredMediaUrl={featuredMedia}
+						featuredMedia={featured_media}
 						updatePost={this.savePost}
+						deletePost={this.removePost}
+						wpUrl={WP_URL}
 						key={post.id}
 					/>
 				);
-			});
-		}
+			}) :
+			'No posts';
 
 		return (
 			<div id="app">

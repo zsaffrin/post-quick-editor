f68cb3cb16bcb5ebdb566dcca568cfed796db4d2
Zach Saffrin <zach@zachsaffrin.com>
2017-08-07 23:05:31 -0400

    Add title support

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 require('../styles/Button.scss');
 
-const Button = ({ type, label, faIcon, action }) => {
+const Button = ({ title, type, label, faIcon, action }) => {
 	let icon;
 	if (faIcon) {
 		icon = <div className={`icon fa fa-${faIcon}`} />;
@@ -15,19 +15,25 @@ const Button = ({ type, label, faIcon, action }) => {
 	}
 
 	return (
-		<button className={`button ${type}`} onClick={action}>
+		<button
+			title={title}
+			className={`button ${type}`}
+			onClick={action}
+		>
 			{icon}
 			{labelDiv}
 		</button>
 	);
 };
 Button.propTypes = {
+	title: PropTypes.string,
 	type: PropTypes.string,
 	label: PropTypes.string,
 	faIcon: PropTypes.string,
 	action: PropTypes.func,
 };
 Button.defaultProps = {
+	title: '',
 	type: 'default',
 	label: '',
 	faIcon: '',

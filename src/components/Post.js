b165b26b06b7c2c6f3475cf58e8e8568ee183a7e
Zach Saffrin <zach@zachsaffrin.com>
2017-08-07 23:05:08 -0400

    Reorganize structure, add Delete Post functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { fetchGetJson } from './_staticFunctions';
+
 import Button from './Button';
 
 require('../styles/Post.scss');
@@ -10,19 +12,35 @@ class Post extends Component {
 		super(props);
 		this.state = {
 			isEditing: false,
+			isConfirmDelete: false,
 			postId: props.id,
 			postTitle: props.title,
-			postMedia: props.featuredMediaUrl,
+			postMedia: {},
 		};
 
 		this.toggleEditing = this.toggleEditing.bind(this);
+		this.toggleConfirmDelete = this.toggleConfirmDelete.bind(this);
 		this.handleTitleChange = this.handleTitleChange.bind(this);
-		this.handleSaveChanges = this.handleSaveChanges.bind(this);
+		this.savePostChanges = this.savePostChanges.bind(this);
+		this.removePost = this.removePost.bind(this);
 		this.handleCancel = this.handleCancel.bind(this);
+	}
+
+	componentDidMount() {
+		const { wpUrl, featuredMedia } = this.props;
+
+		if (this.props.featuredMedia !== 0) {
+			fetchGetJson(`${wpUrl}/media/${featuredMedia}`)
+				.then(obj => this.setState({ postMedia: obj }));
+		}
 	}
 
 	toggleEditing() {
 		this.setState({ isEditing: !this.state.isEditing });
+	}
+
+	toggleConfirmDelete() {
+		this.setState({ isConfirmDelete: !this.state.isConfirmDelete });
 	}
 
 	revertChanges() {
@@ -37,12 +55,17 @@ class Post extends Component {
 		});
 	}
 
-	handleSaveChanges() {
+	savePostChanges() {
 		const { postId, postTitle } = this.state;
 		const body = new window.FormData();
 		body.append('title', postTitle);
 		this.props.updatePost(postId, body);
 		this.toggleEditing();
+	}
+
+	removePost() {
+		this.props.deletePost(this.state.postId);
+		this.toggleConfirmDelete();
 	}
 
 	handleCancel() {
@@ -52,24 +75,25 @@ class Post extends Component {
 
 	handleSubmit(e) {
 		e.preventDefault();
-		this.handleSaveChanges();
+		this.savePostChanges();
 	}
 
 	render() {
-		const { isEditing, postTitle } = this.state;
+		const { isEditing, isConfirmDelete, postTitle } = this.state;
 
-		const imageField = this.state.postMedia ?
+		const imageField = this.state.postMedia.source_url ?
 			(
 				<img
-					src={this.state.postMedia}
+					src={this.state.postMedia.source_url}
 					alt={`${postTitle} featured media`}
 				/>
 			) :
 			'';
 
-		const titleField = isEditing ?
-			(
-				<form onSubmit={this.handleSaveChanges}>
+		let titleField = postTitle;
+		if (isEditing) {
+			titleField = (
+				<form onSubmit={this.savePostChanges}>
 					<input
 						className="post-title-input"
 						type="text"
@@ -77,17 +101,39 @@ class Post extends Component {
 						onChange={this.handleTitleChange}
 					/>
 				</form>
-			) :
-			postTitle;
+			);
+		}
+		if (isConfirmDelete) {
+			titleField = (
+				<div className="danger-text">
+					Delete post {postTitle}?
+				</div>
+			);
+		}
 
-		const actionsField = isEditing ?
-			(
+		let actionsField = (
+			<div className="post-actions">
+				<Button
+					title="Edit"
+					faIcon="pencil"
+					action={this.toggleEditing}
+				/>
+				<Button
+					title="Delete"
+					type="danger"
+					faIcon="times"
+					action={this.toggleConfirmDelete}
+				/>
+			</div>
+		);
+		if (isEditing) {
+			actionsField = (
 				<div className="post-actions">
 					<Button
 						title="Save Changes"
 						type="action"
 						faIcon="check"
-						action={this.handleSaveChanges}
+						action={this.savePostChanges}
 					/>
 					<Button
 						title="Cancel"
@@ -96,22 +142,27 @@ class Post extends Component {
 						action={this.handleCancel}
 					/>
 				</div>
-			) :
-			(
+			);
+		}
+		if (isConfirmDelete) {
+			actionsField = (
 				<div className="post-actions">
 					<Button
-						title="Edit"
-						faIcon="pencil"
-						action={this.toggleEditing}
-					/>
-					<Button
-						title="Delete"
+						title="Delete Post"
+						label="Yes, Delete"
 						type="danger"
 						faIcon="times"
-						action={this.handleCancel}
+						action={this.removePost}
+					/>
+					<Button
+						title="Cancel"
+						label="Cancel"
+						faIcon="ban"
+						action={this.toggleConfirmDelete}
 					/>
 				</div>
 			);
+		}
 
 		return (
 			<div className="post">
@@ -125,14 +176,15 @@ class Post extends Component {
 Post.propTypes = {
 	id: PropTypes.number,
 	title: PropTypes.string,
-	featuredMediaUrl: PropTypes.string,
-	updatePost: PropTypes.func,
+	featuredMedia: PropTypes.number,
+	updatePost: PropTypes.func.isRequired,
+	deletePost: PropTypes.func.isRequired,
+	wpUrl: PropTypes.string.isRequired,
 };
 Post.defaultProps = {
 	id: 0,
 	title: '',
-	featuredMediaUrl: '',
-	updatePost: () => {},
+	featuredMedia: null,
 };
 
 export default Post;

6fba55f204890a25d766cecca493cb6eafab12a1
Zach Saffrin <zach@zachsaffrin.com>
2017-08-09 14:53:27 -0400

    Add tests

--- test/App.test.js
+++ test/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from '../src/components/App';
+import { defaultStateMock } from './mocks/AppMock';
 
 const { describe, it, expect } = global;
 
@@ -11,8 +12,45 @@ describe('<App />', () => {
 		expect(wrapper.exists()).toBe(true);
 	});
 
-	it('should render a div with id \'app\'', () => {
+	it('renders a div with id \'#app\'', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('#app')).toHaveLength(1);
+	});
+
+	it('constructs a default state', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.state).toBeDefined();
+	});
+
+	it('loaded default state matches mock', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.state()).toEqual(defaultStateMock);
+	});
+
+	it('displays title that matches appTitle in state', () => {
+		const wrapper = shallow(<App />);
+		wrapper.setState({ shouldPostsUpdate: false });
+		expect(wrapper.find('.app-title').text()).toEqual(wrapper.state().appTitle);
+	});
+
+	it('title display responds to changes to state', () => {
+		const wrapper = shallow(<App />);
+		const newTitle = 'New test title';
+		wrapper.setState({ shouldPostsUpdate: false });
+		wrapper.setState({ appTitle: newTitle });
+		expect(wrapper.find('.app-title').text()).toEqual(newTitle);
+	});
+
+	it('displays Posts components equal to number of posts in state', () => {
+		const wrapper = shallow(<App />);
+		wrapper.setState({ shouldPostsUpdate: false });
+		expect(wrapper.find('.post')).toHaveLength(0);
+		wrapper.setState({ posts: [
+			{
+				id: 0,
+				title: { rendered: 'New test title' },
+			},
+		] });
+		expect(wrapper.render().find('.post')).toHaveLength(1);
 	});
 });
---
+++ test/Post.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Post from '../src/components/Post';
+import { defaultStateMock } from './mocks/PostMock';
+
+const { describe, it, expect } = global;
+
+const postStem = (
+	<Post
+		id={0}
+		title="test title"
+		featuredMedia={0}
+		updatePost={() => {}}
+		deletePost={() => {}}
+		wpUrl=""
+		key={0}
+	/>
+);
+
+describe('<Post />', () => {
+	it('should shallow mount for testing', () => {
+		const wrapper = shallow(postStem);
+		expect(wrapper.exists()).toBe(true);
+	});
+
+	it('renders a div with class \'.post\'', () => {
+		const wrapper = shallow(postStem);
+		expect(wrapper.find('.post')).toHaveLength(1);
+	});
+
+	it('constructs a default state', () => {
+		const wrapper = shallow(postStem);
+		expect(wrapper.state).toBeDefined();
+	});
+
+	it('default state matches mock', () => {
+		const wrapper = shallow(postStem);
+		expect(wrapper.state()).toEqual(defaultStateMock);
+	});
+
+	it('isEditing state renders post title editing input', () => {
+		const wrapper = shallow(postStem);
+		wrapper.setState({ isEditing: false });
+		expect(wrapper.find('.post-title-input')).toHaveLength(0);
+		wrapper.setState({ isEditing: true });
+		expect(wrapper.find('.post-title-input')).toHaveLength(1);
+	});
+
+	it('isConfirmDelete state renders input', () => {
+		const wrapper = shallow(postStem);
+		wrapper.setState({ isConfirmDelete: false });
+		expect(wrapper.find('.danger-text')).toHaveLength(0);
+		wrapper.setState({ isConfirmDelete: true });
+		expect(wrapper.find('.danger-text')).toHaveLength(1);
+	});
+});
